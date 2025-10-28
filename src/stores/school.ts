7937d07e5d2980ff8477529d429e64e5ff6ea2b4
khoirulariffin <khoirulariffin@gmail.com>
2025-10-28 15:20:53 +0700

    just add hash route

--- src/stores/school.ts
+++ src/stores/school.ts
@@ -21,7 +21,7 @@ export const useSchoolStore = defineStore("school", () => {
 
     try {
       const baseURL = window.location.origin;
-      const response = await fetch(`${baseURL}/school_data.json`);
+      const response = await fetch(`${baseURL}/#/school_data.json`);
       if (!response.ok) {
         throw new Error("Failed to load school data");
       }
